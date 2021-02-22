19ea48f46ae392f12fe30017be49e1894d62252c
MedAziz11 <chagourmedaziz@gmail.com>
2021-02-22 12:35:17 +0100

    init commit

---
+++ detector.py
@@ -0,0 +1,11 @@
+from typing import ByteString
+
+
+class Detector():
+    #class to detect music from a recorded sound
+
+    def __init__(self) -> None:
+        self.recorded = None
+
+    def runRecord(self)-> ByteString:
+        pass
